c8563abdc3fcf68a01c117f0131ee503226d9059
hungdn <trhung1710@gmail.com>
2018-09-14 13:01:51 +0700

    change tileset url

--- homepage.js
+++ homepage.js
@@ -1,7 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-    url : 'http://localhost:8003/tilesets/Keangnam/tileset.json'
+    url : 'https://limitless-river-37913.herokuapp.com/tilesets/Keangnam/tileset.json'
 }));
 
 viewer.zoomTo(tileset, new Cesium.HeadingPitchRange(0, -0.5, 0));
