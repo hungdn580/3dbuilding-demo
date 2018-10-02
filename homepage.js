f46c38d30e8575bc0908e340fec20b15499a228b
hungdn <trhung1710@gmail.com>
2018-10-02 16:23:32 +0700

    add table model des

--- homepage.js
+++ homepage.js
@@ -1,3 +1,5 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZDViMWY1ZS03MWY1LTQ0YjAtYWFiOC03MmUxNzdjZTIzN2YiLCJpZCI6MzY1OSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTUzODQ0NzYwM30.TyPQBHqnVWS_o-innaMJ-KVz4jfGlfx3V2gwAFQyupo';
+
 var viewer = new Cesium.Viewer('cesiumContainer', {
   infoBox : false,
   selectionIndicator : false,
@@ -10,10 +12,24 @@ viewer.scene.globe.depthTestAgainstTerrain = true;
 // viewer.clock.currentTime = new Cesium.JulianDate(2457522.154792);
 var scene = viewer.scene;
 var url = 'https://limitless-river-37913.herokuapp.com/tilesets/Keangnam/tileset_1.json';
+// var url = 'http://localhost:8000/tilesets/Keangnam/tileset_1.json';
+
 var tileset;
 
 var bottomMenu = document.getElementById("bottom-menu");
 var hideMenu = document.getElementById("hide-menu");
+
+// HTML overlay for showing feature name on mouseover
+var nameOverlay = document.createElement('div');
+viewer.container.appendChild(nameOverlay);
+nameOverlay.className = 'backdrop';
+nameOverlay.style.display = 'none';
+nameOverlay.style.position = 'absolute';
+nameOverlay.style.bottom = '0';
+nameOverlay.style.left = '0';
+nameOverlay.style['pointer-events'] = 'none';
+nameOverlay.style.padding = '4px';
+nameOverlay.style.backgroundColor = 'black';
 
 hideMenu.onclick = function(){
     bottomMenu.style.display = "none";
@@ -38,7 +54,8 @@ loadTileset(url);
 
 var canvas = viewer.canvas;
 var handler = new Cesium.ScreenSpaceEventHandler(canvas);
-
+// An entity object which will hold info about the currently selected feature for infobox display
+var selectedEntity = new Cesium.Entity();
 var current = {
     feature : undefined,
     originalColor : new Cesium.Color()
@@ -46,29 +63,48 @@ var current = {
 
 var HIGHLIGHT_COLOR = new Cesium.Color(1.5, 1.0, 0.0, 0.4);
 
-// Highlight feature on mouse over
-handler.setInputAction(function(movement) {
-    var pickedFeature = scene.pick(movement.endPosition);
-    if (Cesium.defined(current.feature) && (current.feature !== pickedFeature)) {
-        // Restore original color to feature that is no longer selected
-        // This assignment is necessary to work with the set property
-        current.feature.color = Cesium.Color.clone(current.originalColor, current.feature.color);
-        current.feature = undefined;
-    } else {
-      // console.log("Feature is not defined");
-    }
-    if (Cesium.defined(pickedFeature) && (pickedFeature !== current.feature)) {
-        current.feature = pickedFeature;
-        Cesium.Color.clone(pickedFeature.color, current.originalColor);
-        // Highlight newly selected feature
-        pickedFeature.color = Cesium.Color.clone(HIGHLIGHT_COLOR, pickedFeature.color);
-        bottomMenu.style.display = "block";
+// Get default left click handler for when a feature is not picked on left click
+var clickHandler = viewer.screenSpaceEventHandler.getInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
-        printProperties(movement, current.feature);
-    } else {
-      // console.log("Feature is not defined");
-    }
-}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+// Highlight feature on mouse over
+viewer.screenSpaceEventHandler.setInputAction(function onLeftClick(movement) {
+  // Pick a new feature
+        var pickedFeature = viewer.scene.pick(movement.position);
+        if (!Cesium.defined(pickedFeature)) {
+          bottomMenu.style.display = "none";
+          // A feature was picked, so show it's overlay content
+          nameOverlay.style.display = 'none';
+          console.log("?????");
+            clickHandler(movement);
+            return;
+        }
+
+        if (current.feature != pickedFeature) {
+          if (current.feature != null) {
+            current.feature.color = Cesium.Color.clone(current.originalColor, current.feature.color);
+            current.feature = undefined;
+          }
+
+          current.feature = pickedFeature;
+          Cesium.Color.clone(pickedFeature.color, current.originalColor);
+          // Highlight newly selected feature
+          pickedFeature.color = Cesium.Color.clone(HIGHLIGHT_COLOR, pickedFeature.color);
+          bottomMenu.style.display = "block";
+          // A feature was picked, so show it's overlay content
+          nameOverlay.style.display = 'block';
+          nameOverlay.style.bottom = viewer.canvas.clientHeight - movement.position.y + 'px';
+          nameOverlay.style.left = movement.position.x + 'px';
+
+          var featureName = pickedFeature.getProperty('name');
+          selectedEntity.name = featureName;
+          selectedEntity.description = 'Loading <div class="cesium-infoBox-loading"></div>';
+          viewer.selectedEntity = selectedEntity;
+
+          printProperties(movement, current.feature);
+        }
+        // current.feature.color = Cesium.Color.clone(current.originalColor, current.feature.color);
+        // current.feature = undefined;
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
 // change style of tile
 // tileset.style = new Cesium.Cesium3DTileStyle({
@@ -89,12 +125,26 @@ function printProperties(movement, feature) {
     console.log('Properties:');
     var propertyNames = feature.getPropertyNames();
     var length = propertyNames.length;
+    var divContent = '<table width="320" border="1"><tbody>';
+
     for (var i = 0; i < length; ++i) {
         var propertyName = propertyNames[i];
+        // if (feature.getProperty('id') == 0) {
+        //   nameOverlay.textContent = 'Keangnam Hanoi Landmark Tower là một khu phức hợp khách sạn-văn phòng-căn hộ-trung tâm thương mại tại đường Phạm Hùng, quận Nam Từ Liêm, Hà Nội được đầu tư xây dựng bởi tập đoàn Keangnam có trụ sở chính tại Dongdaemun-gu, Seoul của Hàn Quốc. Keangnam Landmark Tower được bàn giao từ 20 tháng 3 năm 2011 đến cuối tháng 12 năm 2011 đã có 780 hộ chuyển vào sinh sống [1]. Đây là toà nhà cao nhất Việt Nam từ năm 2010 đến tháng 2-2018.';
+        // } else if (feature.getProperty('id') == 10){
+        //   nameOverlay.textContent = 'Handico Tower là tòa nhà văn phòng hạng A nằm trên ngã tư đường Mễ Trì – Phạm Hùng (thuộc khu đô thị mới Mễ Trì, Quận Nam Từ Liêm, Hà Nội) – tuyến đường có kết nối giao thông quan trọng và đẹp nhất của Thủ đô. Nơi đây được quy hoạch là trung tâm tài chính, hành chính của Hà Nội do Tổng công ty Đầu tư và Phát triển nhà Hà Nội (HANDICO) làm chủ đầu tư. Tòa nhà Handico đã hoàn thành và đưa vào sử dụng năm 2015 được kỳ vọng sẽ tạo được một cú hích trong lĩnh vực cho thuê văn phòng trên địa bàn Thành phố Hà Nội.';
+        // } else if (feature.getProperty('id') == 20) {
+        //   nameOverlay.textContent = 'Tòa nhà CEO Tower nằm trên đường Phạm Hùng, cách trung tâm hội nghị Quốc Gia 200m, nằm giữa khu vực kinh tế sôi động và phát triển nhất khu vực thủ đô Hà Nội. Xung quanh tòa nhà CEO Tower là các dự án quy mô và phát triển: KĐTM Mỹ Đình, KĐTM Nam Trung Yên, KĐTM The Manor, Keangnam Landmark, Vinaconex 9… Có thể nói, tòa nhà là điểm nhấn kiến trúc ở cửa ngõ phía Tây Hà Nội, góp phần làm đẹp cảnh quan khu đô thị mới năng động và chiến lược quy hoạch tổng thể của Thủ đô.';
+        // }
+        divContent += '<tr><th>' + propertyName + '</th><td>' + feature.getProperty(propertyName) + '</td></tr>';
+
         console.log('  ' + propertyName + ': ' + feature.getProperty(propertyName));
     }
+
+    divContent += '</tbody></table>';
+    nameOverlay.innerHTML = divContent;
     // Evaluate feature description
-    console.log('Description : ' + tileset.style.meta.description.evaluate(feature));
+    // console.log('Description : ' + tileset.style.meta.description.evaluate(feature));
 }
 
 // tile failed to load
