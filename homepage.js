be4df84d88add64793431481af463a9509189b26
hungdn <trhung1710@gmail.com>
2018-10-12 22:44:00 +0700

    change http to https

--- homepage.js
+++ homepage.js
@@ -19,7 +19,7 @@ var baseUrl = window.location.hostname;
 console.log(baseUrl);
 
 // var url = 'https://enigmatic-shore-17582.herokuapp.com/tilesets/Keangnam/tileset_1.json';
-var url = 'http://' + baseUrl + '/tilesets/Keangnam/tileset_1.json';
+var url = 'https://' + baseUrl + '/tilesets/Keangnam/tileset_1.json';
 
 var tileset;
 
